bd6ec5783fac443c10cae8654fe1e23f23a2bb97
Florian Ebeling <mail@florianebeling.com>
2023-12-05 16:22:39 +0100

    Set command name

--- options.js
+++ options.js
@@ -1,4 +1,6 @@
-import synopt from './synopt.js';
+import {createCommand} from './synopt.js';
+
+const synopt = createCommand('mkwebmanifest');
 
 synopt
   .summary("Generate icons and web manifest for web applications")
@@ -14,6 +16,6 @@ synopt
   .option("--tags", "print meta tags to include manifest file and icons", { boolean: true })
   .option("--verbose", "print more information the console", { boolean: true })
   .option("--watch", "watch icon and configuration for changes", { boolean: true })
-  .option("-h", "--help", "Print information about options")
+  .option("-h", "--help", "print information about options", { boolean: true })
 
 export { synopt as synoptions };
